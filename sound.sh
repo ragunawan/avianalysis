8f2f9a13d5c04dd72e5cc74f73c1ca96085eca46
boykinbandits <ryanomeara64@gmail.com>
2020-03-09 13:45:56 -0400

    Update sound.sh
    placeholder for moving mp3 to usb drive

--- sound.sh
+++ sound.sh
@@ -12,7 +12,7 @@ FILENAME=$(date +"%Y%m%d_%H%M")
 Export AudioREV=hw:1,0
 
 #Records 30 min of audio
-rec -c 1 -r 44800 -b 16 /home/pi/Audio/Wav/${FILENAME}.wav bandpass 720 600 highpass 120 gain 20 trim 0 1798
+rec -c 1 -r 44800 -b 16 /home/pi/Audio/Wav/${FILENAME}.wav bandpass 720 600 highpass 120 gain 20 trim 2 1802
 
 #converts from .wav to .mp3
 lame -b 64 ./home/pi/Audio/Wav/${FILENAME}.wav ./home/pi/Audio/upload/${FILENAME}.mp3
@@ -20,9 +20,12 @@ lame -b 64 ./home/pi/Audio/Wav/${FILENAME}.wav ./home/pi/Audio/upload/${FILENAME
 #deletes .wav file
 rm /home/pi/Audio/wav/${FILENAME}.WAV
 
+#moves mp3 from SD card to usb drive
+mv
+
 #cellular on
 
-#upload to s3
+#upload to s3 (need to update file paths)
 s3cmd put --reduced-redundancy --acl-public ./home/pi/Audio/upload/${FILENAME}.mp3 s3://MY_S3_BUCKET/${FILENAME}.mp3
 
 #cellular off
